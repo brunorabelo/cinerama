5f08b01e613b5fae57c4ac11db5c07e59af28801
brunorabelo <brunorabelow@hotmail.com>
2019-12-26 21:26:38 -0200

    Link para filme

--- frontend/components/moviecarousel.vue
+++ frontend/components/moviecarousel.vue
@@ -4,9 +4,14 @@
     hide-delimiter-background
     show-arrows-on-hover
   >
+  <router-link v-for="(movie,i) in movies"
+      :key="i" 
+      :to="{name:'filme-movieid',params:{movieid:movie.id}}"
+      
+      tag="a">
+    
     <v-carousel-item
-      v-for="(movie,i) in movies"
-      :key="i"
+      
       :src="movie.backdrop_path"
       
       reverse-transition="fade-transition"
@@ -16,13 +21,14 @@
             <v-container fill-height>
               <v-layout align-end>
                 <v-flex>
-                  <h3 href="/test.html" class="display-3 white--text">{{movie.title}}</h3>
+                  <h3 class="display-3 white--text">{{movie.title}}</h3>
                   <!-- <span class="subheading white--text">Subtítulo filme {{ i + 1}}</span> -->
                 </v-flex>
               </v-layout>
             </v-container>
           </v-responsive>
     </v-carousel-item>
+  </router-link>
   </v-carousel>
 </template>
 
